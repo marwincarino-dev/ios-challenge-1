151cf0a4b656acc483796b5ee414e14d044f1535
Marwin Carino <mcarino.dev@gmail.com>
2024-11-08 12:44:04 +0800

    Move `LocalActivityLoader` to production folder

--- iOSChallenge/iOSChallengeTests/LoadActivityFromLocalUseCaseTests.swift
+++ iOSChallenge/iOSChallengeTests/LoadActivityFromLocalUseCaseTests.swift
@@ -89,36 +89,3 @@ private extension LoadActivityFromLocalUseCaseTests {
         )!
     }
 }
-
-final class LocalActivityLoader: ActivityLoader {
-    private let url: URL
-    private let decoder: JSONDecoder
-    
-    init(url: URL, decoder: JSONDecoder = .localActivityDecoder) {
-        self.url = url
-        self.decoder = decoder
-    }
-
-    func load(completion: @escaping (ActivityLoader.Result) -> Void) {
-        guard url.isFileURL else {
-            completion(.failure(Error.invalidFileURL))
-            return
-        }
-        
-        do {
-            let data = try Data(contentsOf: url)
-            let decodedData = try decoder.decode(ActivityContainer.self, from: data)
-            
-            completion(.success(decodedData))
-        } catch {
-            completion(.failure(Error.invalidData))
-        }
-    }
-}
-
-extension LocalActivityLoader {
-    enum Error: Swift.Error {
-        case invalidData
-        case invalidFileURL
-    }
-}

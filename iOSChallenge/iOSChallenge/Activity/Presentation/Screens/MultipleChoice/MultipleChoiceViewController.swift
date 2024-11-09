17ed74b8078dc519b3a4eb932abfb7e8804bf531
Marwin Carino <mcarino.dev@gmail.com>
2024-11-09 15:29:56 +0800

    Add multiple choice screen

--- iOSChallenge/iOSChallenge/Activity/Presentation/Screens/MultipleChoice/MultipleChoiceViewController.swift
+++ iOSChallenge/iOSChallenge/Activity/Presentation/Screens/MultipleChoice/MultipleChoiceViewController.swift
@@ -8,38 +8,83 @@
 import UIKit
 
 final class MultipleChoiceViewController: UIViewController {
+    // MARK: - IBOutlets
+    
+    @IBOutlet private var questionLabel: UILabel!
+    @IBOutlet private var selectionTypeLabel: UILabel!
+    @IBOutlet private var choicesStackView: UIStackView!
+    @IBOutlet private var continueButton: UIButton!
+    
+    @IBAction
+    private func onTapContinue(_ sender: Any) {
+        onSubmit?()
+    }
+    
+    // MARK: - Properties
+    
+    var onSubmit: (() -> Void)?
     var viewModel: MultipleChoiceViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setup()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    func setup() {
+        questionLabel.text = viewModel.question
+        
+        selectionTypeLabel.isHidden = !viewModel.allowsMultipleChoices
+        
+        continueButton.isHidden = !viewModel.allowsMultipleChoices
+        updateContinueButtonState()
+        
+        setupChoices()
     }
     
-    static func make() -> MultipleChoiceViewController {
-        return UIStoryboard.multipleChoice.instantiateViewController(ofType: MultipleChoiceViewController.self)
+    func setupChoices() {
+        viewModel.choices.forEach { choice in
+            let view = makeChoiceView(with: choice)
+            choicesStackView.addArrangedSubview(view)
+        }
+    }
+    
+    func makeChoiceView(with choice: MultipleChoiceViewModel.Choice) -> UIView {
+        let view = ChoiceView.make()
+        view.configure(emoji: choice.emoji, text: choice.text)
+        view.onTap = handleChoiceOnTap(choice: choice)
+        
+        NSLayoutConstraint.activate([
+            view.heightAnchor.constraint(greaterThanOrEqualToConstant: 54.0)
+        ])
+        
+        return view
+    }
+    
+    func updateContinueButtonState() {
+        guard !continueButton.isHidden else { return }
+        
+        continueButton.isEnabled = !viewModel.selectedChoices.isEmpty
     }
 }
 
-final class MultipleChoiceViewModel: ScreenViewModel {
-    typealias Choice = (id: String, text: String, emoji: String)
-  
-    let id: String
-    let question: String
-    let allowsMultipleChoices: Bool
-    let choices: [Choice]
-    
-    init(
-        id: String,
-        question: String,
-        allowsMultipleChoices: Bool,
-        choices: [Choice]
-    ) {
-        self.id = id
-        self.question = question
-        self.allowsMultipleChoices = allowsMultipleChoices
-        self.choices = choices
+private extension MultipleChoiceViewController {
+    func handleChoiceOnTap(choice: MultipleChoiceViewModel.Choice) -> (() -> Void) {
+        return { [weak self] in
+            guard let self else { return }
+            
+            self.viewModel.select(choice: choice)
+            self.updateContinueButtonState()
+            
+            if !self.viewModel.allowsMultipleChoices {
+                self.onSubmit?()
+            }
+        }
+    }
+}
+
+extension MultipleChoiceViewController {
+    static func make() -> MultipleChoiceViewController {
+        return UIStoryboard.multipleChoice.instantiateViewController(ofType: MultipleChoiceViewController.self)
     }
 }
